86362238bdf751252ffe50195f79d452b0b361d5
luyimin <b37582000@gmail.com>
2016-11-19 17:33:46 +0800

    Update video.rb

--- controllers/video.rb
+++ controllers/video.rb
@@ -3,149 +3,97 @@
 # GroupAPI web service
 class YPBT_API < Sinatra::Base
   YT_URL_REGEX = %r{https://www.youtube.com/watch\?v=(\S[^&]+)}
-  COOLDOWN_TIME = 10 # second
 
-  # Get video info from database
-  # tux: get 'api/v0.1/video/:video_id'
   get "/#{API_VER}/video/:video_id/?" do
-    results = SearchVideo.call(params)
+    video_id = params[:video_id]
+    begin
+      video = Video.find(video_id: video_id)
 
-    if results.success?
-      VideoInfoRepresenter.new(results.value).to_json
-    else
-      ErrorRepresenter.new(results.value).to_status_response
+      content_type 'application/json'
+      { video_id: video_id, title: video.title }.to_json
+    rescue
+      content_type 'text/plain'
+      halt 404, "Video (video_id: #{video_id}) not found"
     end
   end
 
-  # get all tagid for this video
-  get "/#{API_VER}/video/:video_id/get_all_tagid?" do
-    content_type 'application/json'
-    ["Need Implement"].to_json
-  end
-
-  # Add full record of a new video
-  # tux: post 'api/v0.1/video', { url: "youtube_url" }.to_json,
-  #                             'CONTENT_TYPE' => 'application/json'
+  # Body args (JSON) e.g.: {"url": "https://www.youtube.com/watch?v=video_id"}
   post "/#{API_VER}/video/?" do
+    begin
+      body_params = JSON.parse request.body.read
+      yt_video_url = body_params['url']
+      video_id = yt_video_url.match(YT_URL_REGEX)[1]
 
-    result = LoadVideoFromYT.call(request.body.read)
+      if Video.find(video_id: video_id)
+        halt 422, "Video (video_id: #{video_id})already exists"
+      end
 
-    if results.success?
-      VideoInfoRepresenter.new(results.value).to_json
-    else
-      ErrorRepresenter.new(results.value).to_status_response
+      video = YoutubeVideo::Video.find(video_id: video_id)
+    rescue
+      content_type 'text/plain'
+      halt 400, "Video (video_id: #{video_id}) could not be found"
+    end
+
+    begin
+      yt_video = Video.create(video_id: video_id, title: video.title)
+
+      video.commentthreads.each do |comment|
+        Comment.create(
+          video_id:           yt_video.id,
+          comment_id:         comment.comment_id,
+          updated_at:         comment.updated_at,
+          published_at:       comment.published_at,
+          text_display:       comment.text_display,
+          author_name:        comment.author&.author_name,
+          author_image_url:   comment.author&.author_image_url,
+          author_channel_url: comment.author&.author_channel_url,
+          like_count:         comment.author&.like_count
+        )
+      end
+
+      content_type 'application/json'
+      { video_id: yt_video.id, title: yt_video.title }.to_json
+    rescue
+      content_type 'text/plain'
+      halt 500, "Cannot create video (video_id: #{video_id})"
     end
   end
-end
 
-
-  # Update whole record of an existed video in the database
-  # tux: put 'api/v0.1/video/:video_id'
   put "/#{API_VER}/video/:video_id/?" do
     video_id = params[:video_id]
     begin
-      db_video = Video.find(video_id: video_id)
-      halt 400, "Video (video_id: #{video_id}) is not stored" unless db_video
+      video = Video.find(video_id: video_id)
+      halt 400, "Video (video_id: #{video_id}) is not stored" unless video
+      commentthreads = Comment.where(video_id: video.id).all
 
-      time_diff = (Time.now - db_video.last_update_time).to_i
-      if time_diff < COOLDOWN_TIME
-        halt 200, "Already update to lastest"
-      end
-
-      newest_video = YoutubeVideo::Video.find(video_id: video_id)
-      if newest_video.nil?
+      updated_video = YoutubeVideo::Video.find(video_id: video_id)
+      if updated_video.nil?
         halt 404, "Video (video_id: #{video_id}) not found on Youtube"
       end
 
-      db_video.update(
-        title: newest_video.title,
-        description: newest_video.description,
-        view_count: newest_video.view_count,
-        like_count: newest_video.like_count,
-        dislike_count: newest_video.dislike_count,
-        #duration: newest_video.duration,    # Need Revise
-        last_update_time: DateTime.now
-      )
-
-      newest_comments = newest_video.comments
-      newest_comments.each do |newest_comment|
-        db_comment = Comment.find(video_id: db_video.id,
-                                  comment_id: newest_comment.comment_id)
-        if db_comment.nil?
-          new_db_comment = Comment.create(
-            video_id:      db_video.id,
-            comment_id:    newest_comment.comment_id,
-            published_at:  newest_comment.published_at,
-            updated_at:    newest_comment.updated_at ? comment.updated_at : "",
-            text_display:  newest_comment.text_display,
-            like_count:    newest_comment.like_count
-          )
-
-          newest_time_tags = newest_comment.time_tags
-          newest_time_tags.each do |newest_time_tag|
-            existed_time_tag = Timetag.find(comment_id: newest_comment.comment_id,
-                                            start_time: newest_comment.start_time)
-            if existed_time_tag.nil?
-              Timetag.create(
-                comment_id:    new_db_comment.id,
-                yt_like_count: comment.like_count,
-                our_like_count: 0,
-                start_time:    newest_time_tag.start_time,
-              )
-            end
-          end
-
-          newest_author = newest_comment.author
-          Author.create(
-            comment_id:         new_db_comment.id,
-            author_name:        newest_author.author_name,
-            author_image_url:   newest_author.author_image_url,
-            author_channel_url: newest_author.author_channel_url,
-            like_count:         newest_author.like_count
-          )
-        else
-          db_comment.update(
-            published_at:  newest_comment.published_at,
-            updated_at:    newest_comment.updated_at ?
-                           newest_comment.updated_at : "",
-            text_display:  newest_comment.text_display,
-            like_count:    newest_comment.like_count
-          )
-
-          newest_time_tags = newest_comment.time_tags
-          newest_time_tags.each do |newest_time_tag|
-            db_timetag = Timetag.find(comment_id: db_comment.id,
-                                      start_time: newest_time_tag.start_time)
-            if db_timetag.nil?
-              Timetag.create(
-                comment_id:    db_comment.id,
-                yt_like_count: newest_comment.like_count,
-                our_like_count: 0,
-                start_time:    newest_time_tag.start_time,
-              )
-            else
-              db_timetag.update(
-                yt_like_count: newest_comment.like_count
-              )
-            end
-          end
-
-          newest_author = newest_comment.author
-          db_author = Author.find(comment_id: db_comment.id)
-          db_author.update(
-            author_name:        newest_author.author_name,
-            author_image_url:   newest_author.author_image_url,
-            author_channel_url: newest_author.author_channel_url,
-            like_count:         newest_author.like_count
-          )
-        end
+      video.update(video_id: video_id, title: video.title)
+      commentthreads.map do |comment|
+        comment.delete
+      end
+      updated_video.commentthreads.each do |comment|
+        Comment.create(
+          video_id:           video.id,
+          comment_id:         comment.comment_id,
+          updated_at:         comment.updated_at,
+          published_at:       comment.published_at,
+          text_display:       comment.text_display,
+          author_name:        comment.author&.author_name,
+          author_image_url:   comment.author&.author_image_url,
+          author_channel_url: comment.author&.author_channel_url,
+          like_count:         comment.author&.like_count
+        )
       end
 
       content_type 'text/plain'
-      body "Update to lastest"
+      body 'Update to lastest'
     rescue
       content_type 'text/plain'
-      halt 500, "Cannot update posting (id: #{video_id})"
+      halt 500, "Cannot update posting (id: #{posting_id})"
     end
   end
 end
