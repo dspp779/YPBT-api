c0c27b22385e1d849b220bc5a0aaff9a7ba50513
JJC <jjc@nlplab.cc>
2016-12-18 14:01:54 +0800

    the very 1st code refactor

--- lib/comment_record.rb
+++ lib/comment_record.rb
@@ -8,44 +8,36 @@ class CommentRecord
 
   # Create new comment record
   def self.create(comment_info)
-    comment = Comment.create()
+    comment = Comment.create
     update(comment.id, comment_info)
   end
 
   # Find comment record
   def self.find(comment_info)
     comments_found = find_all(comment_info)
-    if comments_found
-      comments_found.first
-    else
-      nil
-    end
+    comments_found.first unless comments_found&.first.nil?
   end
 
   # Find all match comment records
   def self.find_all(comment_info)
     columns = @comment_columns[0..-1]
-    results = Comment.where()
+    results = Comment.where
     columns.each do |col|
       val = comment_info.send(col)
       results = results.where(col => val) unless val.nil?
     end
 
-    render_info = lambda { |result|
-      comment_found = CommentInfo.new()
+    render_info = lambda do |result|
+      comment_found = CommentInfo.new
       columns.each do |col|
-        col_setter = (col.to_s + "=").to_sym
+        col_setter = (col.to_s + '=').to_sym
         column_value = result.send(col)
         comment_found.send(col_setter, column_value)
       end
       comment_found
-    }
-
-    unless results.first.nil?
-      comments_found = results.map &render_info
-    else
-      nil
     end
+
+    results.map(&render_info) unless results.first.nil?
   end
 
   # Update existed comment record
